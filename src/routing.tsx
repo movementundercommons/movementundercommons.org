b178945f042f9240e314017e8476d6b5b78c32cb
Jack Rifkin <jack.rifkin@icloud.com>
2025-07-14 20:53:14 -0400

    use HashRouter

--- src/routing.tsx
+++ src/routing.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router";
+import { createHashRouter } from "react-router";
 import App from "./App";
 
 export enum SectionId {
@@ -6,7 +6,7 @@ export enum SectionId {
   Technology = "Technology",
   Portraits = "Portraits",
   People = "People",
-  Publications = "Publications"
+  Publications = "Publications",
 }
 
 export enum RoutePaths {
@@ -14,8 +14,7 @@ export enum RoutePaths {
   Portraits = "/portraits",
   Technology = "/technology",
   People = "/people",
-  Publications = "/publications"
-
+  Publications = "/publications",
 }
 
 export interface Route {
@@ -28,32 +27,31 @@ export const routes: Array<Route> = [
   {
     id: SectionId.Landing,
     path: RoutePaths.Landing,
-    element: <App sectionId={SectionId.Landing} />
+    element: <App sectionId={SectionId.Landing} />,
   },
   {
     id: SectionId.Portraits,
     path: RoutePaths.Portraits,
-    element: <App sectionId={SectionId.Portraits} />
+    element: <App sectionId={SectionId.Portraits} />,
   },
   {
     id: SectionId.Technology,
     path: RoutePaths.Technology,
-    element: <App sectionId={SectionId.Technology} />
+    element: <App sectionId={SectionId.Technology} />,
   },
   {
     id: SectionId.People,
     path: RoutePaths.People,
-    element: <App sectionId={SectionId.People} />
+    element: <App sectionId={SectionId.People} />,
   },
   {
     id: SectionId.Publications,
     path: RoutePaths.Publications,
-    element: <App sectionId={SectionId.Publications} />
+    element: <App sectionId={SectionId.Publications} />,
   },
-
 ];
 
 /* The following `basename` configuration was used for testing on the development server and 
 should be modified if necessary for the production deployment location */
 // export const router = createBrowserRouter(routes, { basename: import.meta.env.PROD ? "/temp/data-portraits/" : "/" });
-export const router = createBrowserRouter(routes, { basename: "/" });
+export const router = createHashRouter(routes, { basename: "/" });
